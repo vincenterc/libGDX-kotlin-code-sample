00f6bf4448036cbba4769b76c19f4ca74f1a1f8e
vincenterc <chung.menghsun@gmail.com>
2020-08-30 00:13:52 +0800

    Add background guide

--- core/src/com/mygdx/game/MyGdxGame.kt
+++ core/src/com/mygdx/game/MyGdxGame.kt
@@ -4,28 +4,39 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.utils.viewport.ScreenViewport
 
 class MyGdxGame : ApplicationAdapter() {
-    lateinit var batch: SpriteBatch
-    lateinit var img: Texture
+    private lateinit var stage: Stage;
 
     override fun create() {
-        batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        stage = Stage(ScreenViewport())
+        var Help_Guides = 12;
+        var row_height = Gdx.graphics.width / 12;
+        var col_width = Gdx.graphics.height / 12;
+        addBackgroundGuide(Help_Guides)
+    }
+
+    fun addBackgroundGuide(columns: Int) {
+        var texture = Texture(Gdx.files.internal("background.jpg"))
+        texture.setWrap(Texture.TextureWrap.MirroredRepeat, Texture.TextureWrap.MirroredRepeat)
+
+        var textureRegion = TextureRegion(texture)
+        textureRegion.setRegion(0, 0, texture.width * columns, texture.width * columns)
+        var background = Image(textureRegion)
+        background.setSize(Gdx.graphics.width.toFloat(), Gdx.graphics.width.toFloat())
+        background.setPosition(0f, Gdx.graphics.height - background.height)
+        stage.addActor(background)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
-        batch.begin()
-        batch.draw(img, 0f, 0f)
-        batch.end()
-    }
-
-    override fun dispose() {
-        batch.dispose()
-        img.dispose()
+        stage.act()
+        stage.draw()
     }
 }
